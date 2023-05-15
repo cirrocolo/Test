c773ec426004266877218e02c68469a8566eefd6
Henrik Johansson <cirrocolo@outlook.com>
2023-05-15 20:27:54 +0200

    Tidy up!

--- vobchecks.pl
+++ vobchecks.pl
@@ -4,26 +4,16 @@
 # and how to pass arguments to the program in order to select what dummy check/
 # operation to run on the list variables
 
-open(SESAME, "voblist.txt");
-
-while(<SESAME>) {
-    @vobs = split(" ", $_);
-    # @vobs = split("\n", $_);
-}
-
+my @vobs;
 sub list;
+
+open(SESAME, "voblist.txt");
+while(<SESAME>) {@vobs = split(" ", $_);}
 
 if ($ARGV[0] eq "list") {list;}
 
+# List some vob tags
 sub list {
-    # List the vob tags
-    print "Hello from sub list!\n";
-    # while(@vobs) {
-    #    print $_;
     print "$_\n" for @vobs;
     print "1: $vobs[1]\n";
-    # print @vobs;
-    # print "$_" for @vobs;
-    
-    # print @vobs;
 }
